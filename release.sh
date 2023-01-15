a4a37c3ac0af6c79dbbdf27c14897c8139da4d9a
dp111 <19616418+dp111@users.noreply.github.com>
2023-01-15 19:44:18 +0000

    fix line endings

--- release.sh
+++ release.sh
@@ -3,8 +3,7 @@ DSK=ClockSP5.ssd
 BEEB=../MMB_Utils/beeb
 BASICTOOL=../basictool/basictool
 rm -f $DSK
-echo 'CHAIN"ClockSP"' > !BOOT.SRC
-tr '\012' '\015' < !BOOT.SRC >!BOOT
+echo 'CHAIN"ClockSP"' | tr '\012' '\015'  > '!BOOT'
 $BASICTOOL -t ClockSP5.bas ClockSP5
 $BEEB blank_ssd $DSK
 $BEEB opt4 $DSK 3
@@ -12,4 +11,3 @@ $BEEB title $DSK "ClockSP5"
 $BEEB putfile $DSK !BOOT
 $BEEB putfile $DSK ClockSP5
 rm '!BOOT'
-rm '!BOOT.SRC'
